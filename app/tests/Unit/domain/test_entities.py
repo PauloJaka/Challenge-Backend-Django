cc440ab92a60026dfc0a1d8ccf10d07d90668793
PauloJaka <zpaulogamer@gmail.com>
2025-01-29 16:51:27 -0300

    Chore: Unit test refactor, create user and wallet, and add balance on wallet refactor for test and better code

---
+++ app/tests/Unit/domain/test_entities.py
@@ -0,0 +1,21 @@
+import pytest
+from django.utils import timezone
+from domain.entities.user import User
+from domain.entities.wallet import Wallet
+
+@pytest.mark.django_db
+def test_user_creation():
+    user = User(id=1, name="Alice", cpf="12345678909", password="senha_secreta")
+
+    user_created_at = user.created_at.astimezone(timezone.get_current_timezone())
+
+    assert user_created_at.tzinfo == timezone.get_current_timezone()
+
+@pytest.mark.django_db
+def test_wallet_transaction():
+    wallet = Wallet(id=1, cpf="12345678909", balance=100)
+    wallet.add_balance(50)
+
+    wallet_last_transaction_date = wallet.last_transaction_date.astimezone(timezone.get_current_timezone())
+
+    assert wallet_last_transaction_date.tzinfo == timezone.get_current_timezone()
