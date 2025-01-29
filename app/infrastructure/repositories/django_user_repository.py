2e0eebac25be9d8c0ba38074fff0f38069bf68d6
PauloJaka <zpaulogamer@gmail.com>
2025-01-28 22:42:52 -0300

    Fix: created_at on return, test cheking date__joined with is not on the User.

--- app/infrastructure/repositories/django_user_repository.py
+++ app/infrastructure/repositories/django_user_repository.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from domain.entities.user import User as DomainUser
 
+
 class DjangoUserRepository:
     def save(self, user: DomainUser) -> DomainUser:
         DjangoUser = get_user_model()
@@ -13,13 +14,14 @@ class DjangoUserRepository:
             cpf=user.cpf,
             name=user.name,
         )
-        django_user.set_password(user.password)  
+        django_user.set_password(user.password)
         django_user.save()
 
         return DomainUser(
             id=django_user.id,
             name=django_user.name,
             cpf=django_user.cpf,
-            password=django_user.password,  
-            created_at=timezone.localtime(django_user.date_joined),
+            password=django_user.password,
+            created_at=timezone.localtime(django_user.created_at),  
         )
+
